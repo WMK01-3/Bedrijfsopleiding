f8f6b9c640b8f0d5a549866d3e9f3bddcbf1d54d
foepje12 <jellevanommen@hotmail.nl>
2017-11-16 11:40:46 +0100

    Initial Commit

---
+++ BedrijfsOpleiding/BedrijfsOpleiding/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BedrijfsOpleiding
+{
+    public partial class App
+    {
+        private void App_Startup(object sender, StartupEventArgs e)
+        {
+            // Application is running 
+            // Process command line args 
+            var startMinimized = false;
+
+            for (var i = 0; i != e.Args.Length; ++i)
+            {
+                if (e.Args[i] == "/StartMinimized")
+                    startMinimized = true;
+            }
+
+            // Create main application window, starting minimized if specified
+            MainWindow mainWindow = new MainWindow();
+            if (startMinimized)
+            {
+                mainWindow.WindowState = WindowState.Minimized;
+            }
+            mainWindow.Show();
+        }
+    }
+}
