67c35b14935810ba8294e87f61b73eca8fd599b7
foepje12 <jellevanommen@hotmail.nl>
2017-11-20 11:32:00 +0100

    Useless files gone

--- OpleidingenBedrijf/ViewModel/MainWindowVM.cs
+++ OpleidingenBedrijf/ViewModel/MainWindowVM.cs
@@ -1,16 +1,18 @@
-﻿namespace BedrijfsOpleiding.ViewModel
+﻿using BedrijfsOpleiding.Models;
+
+namespace BedrijfsOpleiding.ViewModel
 {
     public class MainWindowVM : BaseViewModel
     {
-       // public User CurUser { get; }
+        public User CurUser { get; private set; }
 
         public MainWindowVM() : base(boundView: null)
         {
         }
 
-        public void SetUser(/*User user*/)
+        public void SetUser(User user)
         {
-            //CurUser = user;
+            CurUser = user;
         }
     }
 }
