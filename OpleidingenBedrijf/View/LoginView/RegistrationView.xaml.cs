a7923024746bbaec0756e6232c3e575f565aa2ea
niels11 <nielssellies11@gmail.com>
2017-11-20 12:57:36 +0100

    Solving merge conflict

--- OpleidingenBedrijf/View/LoginView/RegistrationView.xaml.cs
+++ OpleidingenBedrijf/View/LoginView/RegistrationView.xaml.cs
@@ -1,26 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using BedrijfsOpleiding.ViewModel;
 
 namespace BedrijfsOpleiding.View.LoginView
 {
     /// <summary>
     /// Interaction logic for RegistrationView.xaml
     /// </summary>
-    public partial class RegistrationView : UserControl
+    public partial class RegistrationView : BaseView
     {
-        public RegistrationView()
+        public RegistrationView(BaseViewModel parent) : base(parent)
         {
             InitializeComponent();
         }
