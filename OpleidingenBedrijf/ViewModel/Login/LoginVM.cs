5e9db05af5da90164e1dcf9a42e4bfdcce9239a8
niels11 <nielssellies11@gmail.com>
2017-11-21 14:03:54 +0100

    Push for commit

--- OpleidingenBedrijf/ViewModel/Login/LoginVM.cs
+++ OpleidingenBedrijf/ViewModel/Login/LoginVM.cs
@@ -15,13 +15,22 @@ namespace BedrijfsOpleiding.ViewModel.Login
         public void Login()
         {
             LoginView loginV = (LoginView) CurrentView;
-
+            loginV.Password.BorderBrush = System.Windows.Media.Brushes.CornflowerBlue;
+            loginV.Username.BorderBrush = System.Windows.Media.Brushes.CornflowerBlue;
             loginV.ErrorMessage.Visibility = Visibility.Hidden;
-            if (loginV.Username.Text == "" || loginV.Password.Text == "")
+            if (loginV.Username.Text == "")
             {
                 loginV.ErrorMessage.Visibility = Visibility.Visible;
+                loginV.Username.BorderBrush = System.Windows.Media.Brushes.Red;
                 loginV.ErrorMessageMessage.Content = "Een of meerdere velden zijn leeg";
             }
+            if (loginV.Password.Text == "")
+            {
+                loginV.ErrorMessage.Visibility = Visibility.Visible;
+                loginV.Password.BorderBrush = System.Windows.Media.Brushes.Red;
+                loginV.ErrorMessageMessage.Content = "Een of meerdere velden zijn leeg";
+            }
+            //if(loginV.Username.Text == )
         }
     }
 }
