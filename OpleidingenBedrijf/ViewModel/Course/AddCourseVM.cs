6b1b52c2ab176f3cf03b663397367e8234e00e97
Auke <auke.v.o@hotmail.com>
2017-12-05 11:36:37 +0100

    backend fix * aanpassingen enzo

--- OpleidingenBedrijf/ViewModel/Course/AddCourseVM.cs
+++ OpleidingenBedrijf/ViewModel/Course/AddCourseVM.cs
@@ -88,6 +88,70 @@ namespace BedrijfsOpleiding.ViewModel.Course
 
         public void SaveCourse()
         {
+            _errorCount = 0;
+
+
+            #region ErrorIcons
+
+            _view.ecCourseName.Visibility = (_view.CourseName.Text.Length == 0 ? Visibility.Visible : Visibility.Hidden);
+            _view.ecMaxParticipants.Visibility = (_view.MaxParticipants.Value == 0 ? Visibility.Visible : Visibility.Hidden);
+            //av.ecStartDate.Visibility = (av.StartDate.ToString().Length == 0 ? Visibility.Visible : Visibility.Hidden);
+            _view.ecPrice.Visibility = (_view.Price.Text.Length == 0 ? Visibility.Visible : Visibility.Hidden);
+
+            #endregion
+
+            #region ErrorBorders
+
+            _view.CourseName.BorderBrush = _view.CourseName.Text.Length == 0 ? _redBrush : _blueBrush;
+            _view.MaxParticipants.Background = _view.MaxParticipants.Value == 0 ? _redBrush : _blueBrush;
+            //av.StartDate.BorderBrush = av.StartDate.ToString().Length == 0 ? _redBrush : _blueBrush;
+            _view.Price.BorderBrush = _view.Price.Text.Length == 0 ? _redBrush : _blueBrush;
+
+            #endregion
+
+            #region ErrorCount
+
+            string errorMsg = "All good";
+
+            _errorCount += _view.CourseName.Text.Length == 0 ? 1 : 0;
+            errorMsg = _view.CourseName.Text.Length == 0  ? "Cursusnaam mag niet leeg zijn" : errorMsg;
+
+            _errorCount += _view.MaxParticipants.Value == 0 ? 1 : 0;
+            errorMsg = _view.MaxParticipants.Value == 0 ? "Minimaal 1 cursist moet zich in kunnen schrijven" : errorMsg;
+
+            //_errorCount += av.StartDate.ToString().Length == 0 ? 1 : 0;
+            _errorCount += _view.Price.Text.Length == 0 ? 1 : 0;
+            errorMsg = _view.Price.Text.Length == 0 ? "De prijs mag niet leeg zijn" : errorMsg;
+
+            _errorCount += _view.Price.Text.IsMoney() ? 0 : 1;
+
+
+            _view.ErrorMessageMessage.Content = "wattttt... NO MUNEY????";
+
+            #endregion
+
+            //If everything has some sort of value, continu
+            if (_errorCount != 0)
+            {
+                _view.ErrorMessage.Visibility = Visibility.Visible;
+                return;
+            }
+
+            _view.ErrorMessage.Visibility = Visibility.Hidden;
+
+            Models.Course course = new Models.Course();
+            course.Title = _view.CourseName.Text;
+            course.Difficulty = (Models.Course.DifficultyEnum)_view.Difficulty.SelectedItem;
+            course.MaxParticipants = (int)_view.MaxParticipants.Value;
+            course.Duration = (Models.Course.DurationEnum)_view.Duration.SelectedItem;
+
+            course.Price = decimal.Parse(_view.Price.Text);
+
+            course.Description = new TextRange(_view.Description.Document.ContentStart, _view.Description.Document.ContentEnd).Text;
+            //course.UserID = short.Parse(av.TeacherID.Text);
+            //course.LocationID = int.Parse(av.LocationID.Text);
+            //course.Dates.Add(av.StartDate.);
+
             using (CustomDbContext context = new CustomDbContext())
             {
                 Models.Course oldCourse = (from c in context.Courses
