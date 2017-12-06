cfd018a18ae4cc07dd3751a6f7e9a432c70fd244
DESKTOP-APQK5T5\mikee <mike.edinger@hotmail.com>
2017-12-06 15:43:02 +0100

    Dashboard progress

--- OpleidingenBedrijf/View/DashBoardView.xaml.cs
+++ OpleidingenBedrijf/View/DashBoardView.xaml.cs
@@ -1,21 +1,28 @@
 ﻿
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Forms;
 using BedrijfsOpleiding.ViewModel;
+using GoogleMaps.LocationServices;
 using Newtonsoft.Json;
 using MessageBox = System.Windows.MessageBox;
 using WebBrowser = System.Windows.Controls.WebBrowser;
 
 namespace BedrijfsOpleiding.View
 {
+    [ComVisible(true)]
     public partial class DashBoardView
     {
         #region OwnViewModel : BaseViewModel
 
         private DashBoardVM _viewModel;
+        public static System.Windows.Controls.ListBox ListBox;
 
         public DashBoardVM ViewModel
         {
@@ -24,28 +31,68 @@ namespace BedrijfsOpleiding.View
         }
 
         #endregion
-
         public DashBoardView(MainWindowVM mainVM) : base(mainVM)
         {
             InitializeComponent();
             DataContext = new DashBoardVM(mainVM, this);
+            ListBox = lbCourses;
 
             string curDir = Directory.GetCurrentDirectory();
             Uri url = new Uri(String.Format("file:///{0}/View/GoogleMaps/map.html", curDir));
             wbMaps.Navigate(url);
+
+            wbMaps.ObjectForScripting = new MapsFunctions();
         }
 
         private void wbMaps_LoadCompleted(object sender, System.Windows.Navigation.NavigationEventArgs e)
         {
             wbMaps.InvokeScript("initialize");
-            wbMaps.InvokeScript("alert", "Document loaded and initialized");
-            
-            wbMaps.InvokeScript("addMarker", JsonConvert.SerializeObject(new
-            {
-                Lat = new double[] { 48.209331, 20.0, 30.0 },
-                Long = new double[] { 16.381302, 20.0, 30.0 }
-            }));
 
+            ((DashBoardVM) ViewModel).LoadMarkers(wbMaps);
         }
+
+        private void lbCourses_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+          
+        }
+    }
+
+    public class lbItem
+    {
+        public int Value { get; set; }
+        public string Text { get; set; }
+    }
+
+    [ComVisible(true)]
+    public class MapsFunctions
+    {
+        public static System.Windows.Controls.ListBox ListBox;
+
+        public MapsFunctions()
+        {
+            ListBox = DashBoardView.ListBox;
+        }
+
+        public void LoadCoursesInListBox(int location_id)
+        {
+            using (var context = new CustomDbContext())
+            {
+                var courses = (from c in context.Courses
+                    where c.LocationID == location_id
+                    select c).ToList();
+
+                if (ListBox.Items.Count > 0)
+                {
+                    ListBox.Items.Clear();
+                }
+
+                foreach (var course in courses)
+                {
+                    ListBox.Items.Add(new lbItem() { Value = course.CourseID, Text = course.Title });
+                }
+                ListBox.SelectedIndex = 0;
+            }
+        }
+
     }
 }
--- OpleidingenBedrijf/ViewModel/DashBoardVM.cs
+++ OpleidingenBedrijf/ViewModel/DashBoardVM.cs
@@ -1,4 +1,11 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Windows.Controls;
+using System.Xml;
 using BedrijfsOpleiding.View;
 using Newtonsoft.Json;
 
@@ -8,14 +15,112 @@ namespace BedrijfsOpleiding.ViewModel
     {
         public bool IsCursist => MainVM.IsEmployee == false;
         public bool IsEmployee => MainVM.IsEmployee;
+       // private double[] _longlat = new double[2];
+        private double[] _longlat = new double[2];
+
+
 
         public DashBoardVM(MainWindowVM vm, DashBoardView v) : base(vm)
         {
         }
 
+        private double[] _getLongLat(string street, string city, string zip)
+        {
+            // to Read the Stream
+            StreamReader sr = null;
+
+            //The Google Maps API Either return JSON or XML. We are using XML Here
+            //Saving the url of the Google API 
+            string url = String.Format($"http://maps.googleapis.com/maps/api/geocode/xml?address={street}+{city}+{zip}&sensor=false");
+
+            //to Send the request to Web Client 
+            WebClient wc = new WebClient();
+            try
+            {
+                sr = new StreamReader(wc.OpenRead(url));
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("The Error Occured" + ex.Message);
+            }
+
+            try
+            {
+                XmlTextReader xmlReader = new XmlTextReader(sr);
+                bool latread = false;
+                bool longread = false;
+
+                while (xmlReader.Read())
+                {
+                    xmlReader.MoveToElement();
+                    switch (xmlReader.Name)
+                    {
+                        case "lat":
+
+                            if (!latread)
+                            {
+                                xmlReader.Read();
+                                _longlat[1] = Convert.ToDouble(xmlReader.Value);
+                                Console.WriteLine(_longlat[1]);
+                                latread = true;
+
+                            }
+                            break;
+
+                        case "lng":
+                            if (!longread)
+                            {
+                                xmlReader.Read();
+                                _longlat[0] = Convert.ToDouble(xmlReader.Value);
+                                Console.WriteLine(_longlat[0]);
+                                longread = true;
+                            }
+
+                            break;
+                    }
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("An Error Occured" + ex.Message);
+            }
+
+            Console.WriteLine($"Return _longlat:{_longlat[0] }, {_longlat[1] }");
+
+            return _longlat;
+
+        }
+
+
         public void LoadMarkers(WebBrowser wbMaps)
         {
-            
+        
+            using (var context = new CustomDbContext())
+            {
+                var allLocationIdFromCourses = (from cl in context.Courses
+                    select cl.LocationID).Distinct().ToList();
+
+                foreach (var location_id in allLocationIdFromCourses)
+                {
+                    var location = (from l in context.Locations
+                        where l.LocationID == location_id
+                        select l).First();
+
+                    double Long = _getLongLat(location.Street, location.City, location.Zipcode)[0];
+                    double Lat = _getLongLat(location.Street, location.City, location.Zipcode)[1];
+                    string title = location.Classroom.ToString();
+
+                    Console.WriteLine($"{location.City}: {Long}, {Lat}");
+
+                    wbMaps.InvokeScript("addMarker", JsonConvert.SerializeObject(new
+                    {
+                        Lat = new double[] { Lat },
+                        Long = new double[] { Long }
+                    }), title, location.LocationID);
+                }
+            }
+
         }
     }
 }
