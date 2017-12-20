31e0bfd2b0e1b24d1d7d203789e7eb2b442233cf
niels11 <nielssellies11@gmail.com>
2017-12-20 10:41:24 +0100

    smol fixerino + extra white spaces

--- OpleidingenBedrijf/ViewModel/Message/MessageVM.cs
+++ OpleidingenBedrijf/ViewModel/Message/MessageVM.cs
@@ -11,8 +11,8 @@ namespace BedrijfsOpleiding.ViewModel.Message
     public class MessageVM : BaseViewModel
     {
         private readonly User _user;
-
         public List<Models.Message> MessageList => GetMessageList();
+        public string Title { get; set; }
 
         public MessageVM(MainWindowVM vm, UserControl boundView) : base(vm)
         {
@@ -23,14 +23,16 @@ namespace BedrijfsOpleiding.ViewModel.Message
         {
             using (CustomDbContext context = new CustomDbContext())
             {
-                IQueryable<Models.Message> result = (from m in context.Messages
+                IQueryable<Models.Message> result = from m in context.Messages
                     where m.UserID == _user.UserID
-                    select m).First();
+                    select m;
 
                 foreach (Models.Message message in result)
                 {       
+
                     MessageList.Add(message);
                 }
+
                 return MessageList;
             }
         }
