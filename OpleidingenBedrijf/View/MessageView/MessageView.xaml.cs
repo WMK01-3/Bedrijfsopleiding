c941a88510281ccd95effab4a23120e25fac687e
niels11 <nielssellies11@gmail.com>
2017-12-20 14:55:17 +0100

    Read gets tracked

--- OpleidingenBedrijf/View/MessageView/MessageView.xaml.cs
+++ OpleidingenBedrijf/View/MessageView/MessageView.xaml.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using System.Drawing;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using BedrijfsOpleiding.Models;
+using System.Windows.Forms;
 using BedrijfsOpleiding.ViewModel;
 using BedrijfsOpleiding.ViewModel.Message;
+using Message = BedrijfsOpleiding.Models.Message;
 
 
 namespace BedrijfsOpleiding.View.MessageView
@@ -14,6 +16,7 @@ namespace BedrijfsOpleiding.View.MessageView
     {
         private MessageVM _viewModel;
         private string _title;
+        private bool _read;
         public MessageVM ViewModel
         {
             get => _viewModel = _viewModel ?? new MessageVM(MainVM, this);
@@ -36,13 +39,24 @@ namespace BedrijfsOpleiding.View.MessageView
                 _title = ((Message)Messages.SelectedItem).Title;
                 Title.Content = _title;
                 MessageText.Text = ((Message)Messages.SelectedItem).MessageText;
+                using (CustomDbContext context = new CustomDbContext())
+                {
+                    IQueryable<Message> read = (from message in context.Messages
+                                                where message.MessageID == ((Message)Messages.SelectedItem).MessageID
+                                                select message);
+                    foreach (var m in read)
+                    {
+                        m.Read = true;
+                    }
+                    context.SaveChanges();
+                }
             }
             catch
             {
 
             }
-           
-            
+
+
         }
 
         private void BtnDelete_Click(object sender, RoutedEventArgs e)
@@ -52,15 +66,15 @@ namespace BedrijfsOpleiding.View.MessageView
                 using (CustomDbContext context = new CustomDbContext())
                 {
                     IQueryable<Message> deleteMessage = (from message in context.Messages
-                        where message.MessageID == ((Message)Messages.SelectedItem).MessageID
-                        select message);
+                                                         where message.MessageID == ((Message)Messages.SelectedItem).MessageID
+                                                         select message);
                     context.Messages.Remove(deleteMessage.First());
                     context.SaveChanges();
                     MainVM.CurrentView = new MessageView(MainVM);
 
                 }
             }
-            catch{} 
+            catch { }
         }
     }
 }
